ea93c8d0128b2aebac6e5f5fcf7bc252335420a0
Luca Bellenghi <lucabel@gmail.com>
2024-12-12 15:24:22 +0100

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.volto.subfooter",
-    version="1.1.1",
+    version="1.1.2.dev0",
     description="Add-on for Volto to manage a subfooter in dropdown one.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
